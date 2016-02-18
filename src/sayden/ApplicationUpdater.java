6553e161423315b20b217e30975511a5c37177a8
Bruno Matias Balderrabano <bmbalderrabano@gmail.com>
2016-02-18 10:44:32 -0300

    Update testing

--- src/sayden/ApplicationUpdater.java
+++ src/sayden/ApplicationUpdater.java
@@ -1,6 +1,9 @@
 package sayden;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -37,28 +40,52 @@ public class ApplicationUpdater {
 	
 	public int currentRevision(){
         BufferedReader is;
+        
         try {
             is = new BufferedReader(
                     new InputStreamReader(ClassLoader.getSystemResource("version.txt").openStream()));
             int rev = Integer.valueOf(is.readLine());
+            
+            is.close();
+            
             return rev;
         } catch(NullPointerException e){
         }catch (Exception e) {
             e.printStackTrace();
         }
+        
         return 1<<31-1;
+    }
+	
+	public void modifyRevision(int version){
+		File local_version = new File("src/version.txt");
+        BufferedWriter bw;
+        
+        try {
+            bw = new BufferedWriter(new FileWriter(local_version));
+            bw.append(version + "");
+            bw.close();
+        } catch(NullPointerException e){
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 	public void checkUpdate() {
-		if(latestRevision() > currentRevision()){
+		int latestRevision = latestRevision();
+		
+		if(latestRevision > currentRevision()){
 			Updater up = new Updater("https://raw.githubusercontent.com/BBalderrabano/SimpleSayden/master/deploy/Sayden.jar");
 	        up.downloadLatestVersion();
+	        
 	        try {
 	            @SuppressWarnings("unused")
 				Process foo = Runtime.getRuntime().exec("java -jar Sayden.jar");
+	            modifyRevision(latestRevision);
 	        } catch (IOException e) {
 	            e.printStackTrace();
 	        }
+	        
 	        up.dispose();
 		}
 	}
